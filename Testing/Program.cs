30d05327327ce3c7e48e8bb10940fadc1d9f53d1
Margarito <margarito.jimenez@developers.net>
2025-08-13 11:08:07 -0600

    UNIENDO ULTIMOS 2 COMMITS

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -6,8 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+var app1 = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Testing/WeatherForecast.cs
+++ Testing/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace Testing
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        public int TemperatureC1 { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
